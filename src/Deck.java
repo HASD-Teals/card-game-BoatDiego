18e3436e8ee688a4e90db9572df026ec5e599b90
BoatNasa25 <Diegovera2511@gmail.com>
2022-01-28 10:02:27 -0600

    fdf

--- src/Deck.java
+++ src/Deck.java
@@ -1,9 +1,30 @@
+import javax.sound.sampled.BooleanControl;
+
 public class Deck {
     // PROPERTIES
+    private Card[] defCard = new Card[52];
     private Card[] cards;
 
     // CONSTRUCTORS
-    public Deck() {}
+    public Deck() {
+
+        this.cards = this.defCard;
+    }
+
+    /**
+     * declares custom deck object
+     * 
+     * @param numberOfCardsPerSuit
+     * @param HighAce
+     * @param hearts
+     * @param spades
+     * @param cloves
+     * @param diamonds
+     */
+    public Deck(int numberOfCardsPerSuit, boolean HighAce, boolean hearts, boolean spades, boolean cloves,
+            boolean diamonds) {
+
+    }
 
     // ACCESSORS
     public Card[] getCards() {
@@ -19,4 +40,22 @@ public class Deck {
     public void shuffleCards() {
         // Shuffle this.cards in a random order
     }
+    /*
+     * 52 cards - final amount of cards in the default array
+     * 26 red and black - if this suit, use black or red
+     * 4 suits - for loop start
+     * 13 cards per suit - fill different suit when loop is over
+     * king, queen, and jack are all value of 10 - if these values
+     * ace is value of 1
+     */
+
+    public Card[] defDeck() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+
+            }
+        }
+
+    }
+
 }
