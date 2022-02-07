aa3c2688ea15a815f76815b195f2938aa1631e97
BoatNasa25 <Diegovera2511@gmail.com>
2022-02-07 10:03:57 -0600

    sfdasfadjkl

--- src/CardGameClient.java
+++ src/CardGameClient.java
@@ -1,5 +1,14 @@
 public class CardGameClient {
     public static void main(String[] args) throws Exception {
-        
+        Deck One = new Deck();
+        Deck Two = new Deck();
+        One.shuffleCards();
+        Two.shuffleCards();
+    
+        for (int i = 0; i < 52; i++) {
+           if (1==1){}
+        }
+
     }
 }
+
